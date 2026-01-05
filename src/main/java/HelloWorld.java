321c8d2c71cfb5ee3c9d9f95a8b7e6f9ceed004f
Nagaraj Reddy <nagaraj.reddy@kyndryl.com>
2026-01-05 16:39:20 +0530

    First commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        // Display the string "Hello, World"
+        System.out.println("Hello, World");
+    }
+}
